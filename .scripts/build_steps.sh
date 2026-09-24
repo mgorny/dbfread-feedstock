4cf61d7349d42faef96c9c0203db0e9a2186b948
Michał Górny <mgorny@quansight.com>
2026-09-24 15:11:14 +0200

    MNT: Re-rendered with conda-smithy 0.0.0 and conda-forge-pinning 2026.09.24.04.24.45
    Other tools:
    - conda-build 26.7.1
    - rattler-build 0.76.1
    - rattler-build-conda-compat 1.4.19

--- .scripts/build_steps.sh
+++ .scripts/build_steps.sh
@@ -9,17 +9,23 @@
 
 set -xeuo pipefail
 export FEEDSTOCK_ROOT="${FEEDSTOCK_ROOT:-/home/conda/feedstock_root}"
+# ensure absolute path
+FEEDSTOCK_ROOT=$(cd "${FEEDSTOCK_ROOT}"; pwd)
 source ${FEEDSTOCK_ROOT}/.scripts/logging_utils.sh
 
 
 ( endgroup "Start Docker" ) 2> /dev/null
 
-( startgroup "Configuring conda" ) 2> /dev/null
+( startgroup "Configuring build environment" ) 2> /dev/null
 
+MINIFORGE_HOME=/opt/conda
 export PYTHONUNBUFFERED=1
 export RECIPE_ROOT="${RECIPE_ROOT:-/home/conda/recipe_root}"
+# ensure absolute path
+RECIPE_ROOT=$(cd "${RECIPE_ROOT}"; pwd)
 export CI_SUPPORT="${FEEDSTOCK_ROOT}/.ci_support"
 export CONFIG_FILE="${CI_SUPPORT}/${CONFIG}.yaml"
+export PIXI_CACHE_DIR="${MINIFORGE_HOME}"
 export RATTLER_CACHE_DIR="${FEEDSTOCK_ROOT}/build_artifacts/pkg_cache"
 
 cat >~/.condarc <<CONDARC
@@ -28,33 +34,52 @@ conda-build:
   root-dir: ${FEEDSTOCK_ROOT}/build_artifacts
 pkgs_dirs:
   - ${FEEDSTOCK_ROOT}/build_artifacts/pkg_cache
-  - /opt/conda/pkgs
+  - ${MINIFORGE_HOME}/pkgs
 solver: libmamba
 
 CONDARC
-mv /opt/conda/conda-meta/history /opt/conda/conda-meta/history.$(date +%Y-%m-%d-%H-%M-%S)
-echo > /opt/conda/conda-meta/history
-micromamba install --root-prefix ~/.conda --prefix /opt/conda \
-    --yes --override-channels --channel conda-forge --strict-channel-priority \
+
+( endgroup "Configuring build environment" ) 2> /dev/null
+
+( startgroup "Provisioning base env with micromamba" ) 2> /dev/null
+
+MAMBA_ROOT_PREFIX=~/.conda
+MICROMAMBA_EXE=micromamba
+
+echo Creating environment
+mv "${MINIFORGE_HOME}"/conda-meta/history "${MINIFORGE_HOME}"/conda-meta/history.$(date +%Y-%m-%d-%H-%M-%S)
+echo > "${MINIFORGE_HOME}"/conda-meta/history
+"${MICROMAMBA_EXE}" install --yes --root-prefix "${MAMBA_ROOT_PREFIX}" --prefix "${MINIFORGE_HOME}" \
+    --override-channels --channel conda-forge --strict-channel-priority \
     pip  rattler-build conda-forge-ci-setup=4 "conda-build>=26.3"
+
+( endgroup "Provisioning base env with micromamba" ) 2> /dev/null
+
+( startgroup "Configuring conda" ) 2> /dev/null
 export CONDA_LIBMAMBA_SOLVER_NO_CHANNELS_FROM_INSTALLED=1
 
-# set up the condarc
+
+
+# Set basic configuration
+echo Setting up configuration
 setup_conda_rc "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
 
+# Linux-specific setup
+echo Running build setup
 source run_conda_forge_build_setup
 
 
-
-# make the build number clobber
-make_build_number "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
 
 
 
 ( endgroup "Configuring conda" ) 2> /dev/null
 
+# make the build number clobber
+make_build_number "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
+
 if [[ -f "${FEEDSTOCK_ROOT}/LICENSE.txt" ]]; then
-  cp "${FEEDSTOCK_ROOT}/LICENSE.txt" "${RECIPE_ROOT}/recipe-scripts-license.txt"
+    echo Copying feedstock license
+    cp "${FEEDSTOCK_ROOT}/LICENSE.txt" "${RECIPE_ROOT}/recipe-scripts-license.txt"
 fi
 
 if [[ "${BUILD_WITH_CONDA_DEBUG:-0}" == 1 ]]; then
@@ -67,51 +92,52 @@ if [[ "${BUILD_WITH_CONDA_DEBUG:-0}" == 1 ]]; then
     export CONDA_BLD_PATH="${CONDA_BLD_PATH:-${FEEDSTOCK_ROOT}/build_artifacts}"
     rattler-build debug setup \
         --recipe "${RECIPE_ROOT}" \
-        -m "${CI_SUPPORT}/${CONFIG}.yaml" \
+        -m "${CONFIG_FILE}" \
         ${EXTRA_CB_OPTIONS:-} \
         ${BUILD_OUTPUT_ID:+--output-name "${BUILD_OUTPUT_ID}"} \
         --build-platform "${BUILD_PLATFORM}" \
         --target-platform "${HOST_PLATFORM}"
 
     rattler-build debug shell
-else
-    # differences between conda-build vs. rattler-build
-    #   - recipe is positional vs. --recipe "${RECIPE_ROOT}"
-    #   - --suppress-variables vs. none
-    #   - --clobber-file vs. none
-    #   - none vs. --target-platform
-    #   - --extra-meta a=b c=d vs. --extra-meta a=b --extra-meta c=d
+    exit 0
+fi
 
-    rattler-build build \
-        --recipe "${RECIPE_ROOT}" \
-        -m "${CI_SUPPORT}/${CONFIG}.yaml" \
-        ${EXTRA_CB_OPTIONS:-} \
-        --build-platform "${BUILD_PLATFORM}" \
-        --target-platform "${HOST_PLATFORM}" \
-        --extra-meta flow_run_id="${flow_run_id:-}" \
-        --extra-meta remote_url="${remote_url:-}" \
-        --extra-meta sha="${sha:-}"
-    ( startgroup "Inspecting artifacts" ) 2> /dev/null
+# Build the recipe
+echo Building recipe
 
-    # inspect_artifacts was only added in conda-forge-ci-setup 4.9.4
-    command -v inspect_artifacts >/dev/null 2>&1 && inspect_artifacts --recipe-dir "${RECIPE_ROOT}" -m "${CONFIG_FILE}" || echo "inspect_artifacts needs conda-forge-ci-setup >=4.9.4"
+# differences between conda-build vs. rattler-build
+#   - recipe is positional vs. --recipe "${RECIPE_ROOT}"
+#   - --suppress-variables vs. none
+#   - --clobber-file vs. none
+#   - none vs. --target-platform
+#   - --extra-meta a=b c=d vs. --extra-meta a=b --extra-meta c=d
 
-    ( endgroup "Inspecting artifacts" ) 2> /dev/null
-    ( startgroup "Validating outputs" ) 2> /dev/null
+rattler-build build \
+    --recipe "${RECIPE_ROOT}" \
+    -m "${CONFIG_FILE}" \
+    ${EXTRA_CB_OPTIONS:-} \
+    --build-platform "${BUILD_PLATFORM}" \
+    --target-platform "${HOST_PLATFORM}" \
+    --extra-meta flow_run_id="${flow_run_id:-}" \
+    --extra-meta remote_url="${remote_url:-}" \
+    --extra-meta sha="${sha:-}"
 
-    validate_recipe_outputs "${FEEDSTOCK_NAME}"
+( startgroup "Inspecting artifacts" ) 2> /dev/null
 
-    ( endgroup "Validating outputs" ) 2> /dev/null
+# inspect_artifacts was only added in conda-forge-ci-setup 4.9.4
+command -v inspect_artifacts >/dev/null 2>&1 && inspect_artifacts --recipe-dir "${RECIPE_ROOT}" -m "${CONFIG_FILE}" || echo "inspect_artifacts needs conda-forge-ci-setup >=4.9.4"
 
+( endgroup "Inspecting artifacts" ) 2> /dev/null
+( startgroup "Validating outputs" ) 2> /dev/null
+validate_recipe_outputs "${FEEDSTOCK_NAME}"
+( endgroup "Validating outputs" ) 2> /dev/null
+
+if [[ "${UPLOAD_PACKAGES}" != "False" ]] && [[ "${IS_PR_BUILD}" == "False" ]]; then
     ( startgroup "Uploading packages" ) 2> /dev/null
-
-    if [[ "${UPLOAD_PACKAGES}" != "False" ]] && [[ "${IS_PR_BUILD}" == "False" ]]; then
-        upload_package --validate --feedstock-name="${FEEDSTOCK_NAME}"  "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
-    fi
-
+    upload_package --validate --feedstock-name="${FEEDSTOCK_NAME}"  "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
     ( endgroup "Uploading packages" ) 2> /dev/null
 fi
 
-( startgroup "Final checks" ) 2> /dev/null
+( startgroup "Leaving the Docker container" ) 2> /dev/null
 
 touch "${FEEDSTOCK_ROOT}/build_artifacts/conda-forge-build-done-${CONFIG}"
--- .scripts/run_win_build.bat
+++ .scripts/run_win_build.bat
@@ -16,7 +16,14 @@ FOR %%A IN ("%~dp0.") DO SET "REPO_ROOT=%%~dpA"
 if "%MINIFORGE_HOME%"=="" set "MINIFORGE_HOME=%USERPROFILE%\Miniforge3"
 :: Remove trailing backslash, if present
 if "%MINIFORGE_HOME:~-1%"=="\" set "MINIFORGE_HOME=%MINIFORGE_HOME:~0,-1%"
+
+set FEEDSTOCK_ROOT=%cd%
+set RECIPE_ROOT=%FEEDSTOCK_ROOT%\recipe
+set CI_SUPPORT=%FEEDSTOCK_ROOT%\.ci_support
+set CONFIG_FILE=%CI_SUPPORT%\%CONFIG%.yaml
+
 call :start_group "Provisioning base env with micromamba"
+
 set "MAMBA_ROOT_PREFIX=%MINIFORGE_HOME%-micromamba-%RANDOM%"
 set "MICROMAMBA_VERSION=1.5.10-0"
 set "MICROMAMBA_URL=https://github.com/mamba-org/micromamba-releases/releases/download/%MICROMAMBA_VERSION%/micromamba-win-64"
@@ -33,9 +40,11 @@ call "%MICROMAMBA_EXE%" create --yes --root-prefix "%MAMBA_ROOT_PREFIX%" --prefi
     --channel conda-forge ^
     pip rattler-build conda-forge-ci-setup=4 "conda-build>=26.3"
 if !errorlevel! neq 0 exit /b !errorlevel!
-echo Removing %MAMBA_ROOT_PREFIX%
+
+echo Cleaning up micromamba
 del /S /Q "%MAMBA_ROOT_PREFIX%" >nul
 del /S /Q "%MICROMAMBA_TMPDIR%" >nul
+
 call :end_group
 
 call :start_group "Configuring conda"
@@ -51,33 +60,41 @@ set "CONDA_LIBMAMBA_SOLVER_NO_CHANNELS_FROM_INSTALLED=1"
 
 :: Set basic configuration
 echo Setting up configuration
-setup_conda_rc .\ ".\recipe" .\.ci_support\%CONFIG%.yaml
-if !errorlevel! neq 0 exit /b !errorlevel!
-echo Running build setup
+setup_conda_rc "%FEEDSTOCK_ROOT%" "%RECIPE_ROOT%" "%CONFIG_FILE%"
+if !errorlevel! neq 0 exit /b !errorlevel!echo Running build setup
 CALL run_conda_forge_build_setup
 
 
 if !errorlevel! neq 0 exit /b !errorlevel!
 
-if EXIST LICENSE.txt (
+call :end_group
+
+if EXIST %FEEDSTOCK_ROOT%\LICENSE.txt (
     echo Copying feedstock license
-    copy LICENSE.txt "recipe\\recipe-scripts-license.txt"
+    copy "%FEEDSTOCK_ROOT%\\LICENSE.txt" "%RECIPE_ROOT%\\recipe-scripts-license.txt"
 )
 
 if NOT [%flow_run_id%] == [] (
         set "EXTRA_CB_OPTIONS=%EXTRA_CB_OPTIONS% --extra-meta flow_run_id=%flow_run_id% --extra-meta remote_url=%remote_url% --extra-meta sha=%sha%"
 )
 
-call :end_group
-
 :: Build the recipe
 echo Building recipe
-rattler-build.exe build --recipe "recipe" -m .ci_support\%CONFIG%.yaml %EXTRA_CB_OPTIONS% --build-platform %BUILD_PLATFORM% --target-platform %HOST_PLATFORM%
+
+rattler-build.exe build ^
+    --recipe "%RECIPE_ROOT%" ^
+    -m %CONFIG_FILE% ^
+    %EXTRA_CB_OPTIONS% ^
+    --build-platform %BUILD_PLATFORM% ^
+    --target-platform %HOST_PLATFORM%
+
 if !errorlevel! neq 0 exit /b !errorlevel!
 
 call :start_group "Inspecting artifacts"
+
 :: inspect_artifacts was only added in conda-forge-ci-setup 4.9.4
-WHERE inspect_artifacts >nul 2>nul && inspect_artifacts --recipe-dir ".\recipe" -m .ci_support\%CONFIG%.yaml || echo "inspect_artifacts needs conda-forge-ci-setup >=4.9.4"
+WHERE inspect_artifacts >nul 2>nul && inspect_artifacts --recipe-dir "%RECIPE_ROOT%" -m %CONFIG_FILE% || echo "inspect_artifacts needs conda-forge-ci-setup >=4.9.4"
+
 call :end_group
 
 :: Prepare some environment variables for the upload step
@@ -101,8 +118,6 @@ if /i "%CI%" == "azure" (
     )
     set "TEMP=%UPLOAD_TEMP%"
 )
-
-:: Validate
 call :start_group "Validating outputs"
 validate_recipe_outputs "%FEEDSTOCK_NAME%"
 if !errorlevel! neq 0 exit /b !errorlevel!
@@ -113,7 +128,7 @@ if /i "%UPLOAD_PACKAGES%" == "true" (
         call :start_group "Uploading packages"
         if not exist "%TEMP%\" md "%TEMP%"
         set "TMP=%TEMP%"
-        upload_package --validate --feedstock-name="%FEEDSTOCK_NAME%" .\ ".\recipe" .ci_support\%CONFIG%.yaml
+        upload_package --validate --feedstock-name="%FEEDSTOCK_NAME%" "%FEEDSTOCK_ROOT%" "%RECIPE_ROOT%" "%CONFIG_FILE%"
         if !errorlevel! neq 0 exit /b !errorlevel!
         call :end_group
     )
